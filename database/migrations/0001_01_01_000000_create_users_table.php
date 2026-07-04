d2119272b8f466ec2ccf234604bef0460fad1954
Liem Phoebe Abigail <phoebeabby05@gmail.com>
2026-07-04 22:56:38 +0700

    modified:   database/migrations/0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role',['admin','member'])->default('member');
-            $table->string('role')->default('member');
             $table->rememberToken();
             $table->timestamps();
         });
